c0239ba705cdc999afe86567fd323e48cbc1e639
Polly Chang <polly_chang@kunyutech.com.tw>
2021-08-27 11:59:32 +0800

    chore(init): init project

---
+++ LeekChain/Blockchain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+namespace LeekChain
+{
+    public class Blockchain
+    {
+        public IList<Block> Chain { set; get; }
+
+        public Block CreateGenesisBlock()
+        {
+            return new Block(DateTime.UtcNow, null, "{}");
+        }
+
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[^1];
+        }
+        
+        public void AddBlock(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Hash = block.CalculateHash();
+            Chain.Add(block);
+        }
+
+        public bool IsValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+                if(currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if(currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+
+        public Blockchain()
+        {
+            InitializeChain();
+            CreateGenesisBlock();
+            AddGenesisBlock();
+        }
+    }
+}
---
+++ LeekChain/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+
+namespace LeekChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Blockchain leakCoin = new Blockchain();
+
+            leakCoin.AddBlock(new Block(DateTime.UtcNow, null, "a quick brown fox"));
+            leakCoin.AddBlock(new Block(DateTime.UtcNow, null, "jumped over"));
+            leakCoin.AddBlock(new Block(DateTime.UtcNow, null, "a lazy dog"));
+
+            var json = JsonSerializer.Serialize(leakCoin, new JsonSerializerOptions() { WriteIndented = true });
+
+            Console.WriteLine(json);
+            Console.ReadLine();
+        }
+    }
+
+}
